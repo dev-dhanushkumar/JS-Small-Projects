4be6c693d1bbc93493b59d443e5d6d55e32f8d9e
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-02-25 14:49:44 +0300

    initial commit

---
+++ Astro-SSR-Blog/src/constants.ts
@@ -0,0 +1,6 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SSITE_DESCRIPTION = 
+    'Articles, stories, and tutorials from Tech people ';
+
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const  ARTICLES_PER_PAGE = 6;
